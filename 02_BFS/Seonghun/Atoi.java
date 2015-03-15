8f480890c28647ebf144d9bebe62031042d121e9
Seonghun Gim <dingyo777@gmail.com>
2015-03-16 01:21:52 +0900

    atoi 문제 해결. 옛날 FSM  비슷하게 해봄. 근데 뭔가 코드가 좀 덕지덕지한 느낌도 있음.

---
+++ 02_BFS/Seonghun/Atoi.java
@@ -0,0 +1,75 @@
+public class Solution {
+    
+    private enum State{
+        BEFORE,
+        SIGN,
+        INT
+    }
+    
+    public int atoi(String str) {
+        
+        State state = State.BEFORE;
+        
+        char ch;
+        
+        int size = str.length();
+        int idx = 0;
+        int sign = 1;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(idx < size){
+            ch = str.charAt(idx);
+            switch(state){
+                case BEFORE:
+                    if('0' <= ch && ch <= '9'){
+                        sb.append(ch);
+                        state = State.INT;
+                    }
+                    else if(ch == '+'){
+                        sb.append(ch);
+                        state = State.SIGN;
+                    }
+                    else if(ch == '-'){
+                        sb.append(ch);
+                        state = State.SIGN;
+                        sign = -1;
+                    }
+                    else if(ch != ' '){
+                        return 0;
+                    }
+                    break;
+                case SIGN:
+                    if('0' <= ch && ch <= '9'){
+                        sb.append(ch);
+                        state = State.INT;
+                    }
+                    else {
+                        return 0;
+                    }
+                    break;
+                case INT:
+                    if('0' <= ch && ch <= '9'){
+                        sb.append(ch);
+                    }
+                    else {
+                        idx = size; // for escaping loop
+                    }
+                    break;
+                default:
+                    break;
+            }
+            idx++;
+        }
+        
+        try{
+            if(sb.length() > 0)   return Integer.parseInt(sb.toString());
+            else                  return 0;    
+        }
+        catch(NumberFormatException e){ 
+            return (sign == 1)? Integer.MAX_VALUE:Integer.MIN_VALUE;
+        }
+        
+    }
+
+}
