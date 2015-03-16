97faed2d7bb39bbb8f912c8779b9cb7218b22b80
JongYoon Ahn <gmggae@gmail.com>
2015-03-16 21:24:42 +0900

    DFS Graph 추

--- 01_DFS/JongYoon/DFS.java
+++ 01_DFS/JongYoon/DFS.java
@@ -1,6 +1,67 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import single_num.Graph;
 import single_num.Graph.Node;
 import single_num.Graph.State;
+
+
+class Graph<T> {
+
+	HashMap<T, Node> graph = new HashMap<T, Node>();
+
+	public enum State {
+		Unvisited, Visited;
+	}
+
+	class Node<T> {
+
+		T value;
+		ArrayList<Node> nodes;
+		State state;
+		int visitCtn = 0;
+
+		public Node(T value) {
+			this.value = value;
+			nodes = new ArrayList<Node>();
+		}
+
+		public Node(Node value) {
+			nodes = new ArrayList<Node>();
+			nodes.add(value);
+			visitCtn++;
+
+		}
+
+		public void insertLink(Node node) {
+			if (nodes != null) {
+				nodes.add(node);
+				visitCtn++;
+			}
+		}
+		
+		public Node getNode(int index){
+			return nodes.get(index);
+		}
+	}
+
+	public Node insertNode(T value) {
+		if (graph.containsKey(value)) {
+			return graph.get(value);
+		} else {
+			Node newNode = new Node(value);
+			graph.put(value, newNode);
+			return newNode;
+		}
+	}
+
+	public void linkingNode(T source, T target) {
+		Node sourceNode = insertNode(source);
+		sourceNode.insertLink(insertNode(target));
+	}
+	
+	
+}
 
 class DFS<T> {
 
