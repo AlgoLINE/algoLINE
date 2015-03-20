6e1d88c30e7378e5cfa0243df8907876f4036858
jongyoun.ahn <jongyoun.ahn@linecorp.com>
2015-03-20 17:19:51 +0900

    first missing positive 풀이. 무식하지만 빠르다.

---
+++ 04_study/JongYoon/FirstMissingPositive.c
@@ -0,0 +1,24 @@
+int firstMissingPositive(int A[], int n) {
+    bool *posNum;
+    int i, num;
+    
+    if(n == 0){
+        return 1;
+    }
+    
+    posNum = (bool*)malloc(sizeof(bool)*n);
+    memset(posNum, 0, n);
+    
+    for(i = 0 ; i < n ; i++){
+        num = A[i];
+        if( num > 0) posNum[num] = true;
+    }
+    
+    for(int i = 1 ; i < INT_MAX ; i++){
+        if(!posNum[i]){
+            return i;
+        }
+    }
+    
+    return 1;
+}
