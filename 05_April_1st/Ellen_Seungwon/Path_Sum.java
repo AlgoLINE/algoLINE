edfd069a7d3b278fe5d5d8e4a2e65152cbb8c206
LinePlus <lineplus@LinePlusui-MacBook-Pro-4.local>
2015-04-11 18:38:48 +0900

    first commit T_T

---
+++ 05_April_1st/Ellen_Seungwon/Path_Sum.java
@@ -0,0 +1,41 @@
+tion for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+public class Solution {
+    int sumPath = 0;
+    boolean flag = false;
+    
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+            
+        sumPath += root.val;
+        
+        if(sumPath==sum && root.left == null && root.right == null){
+            return true;
+        }
+        
+        if(root.left != null){
+            
+            flag = hasPathSum(root.left, sum);
+        } 
+        
+        if(root.right != null) {
+            flag = hasPathSum(root.right, sum);
+        }
+        
+        
+        if(flag) {
+            return true;
+        } else {
+            sumPath -= root.val;
+            return false;
+        }
+    }
+}
