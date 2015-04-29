e9c15e6bf2bd164e94efcb36337ec22e220f8bea
Seungwon Lee <swsw.lee@linecorp.com>
2015-04-30 01:43:17 +0900

    07_study

---
+++ 07_study/seungWon/Remove_Nth_Node.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int len = 0;
+
+        ListNode tmp = head;
+        ListNode next;
+        ListNode prev;
+        List<ListNode> storage = new ArrayList<ListNode>();
+        
+        if(head.next == null){
+            return null;
+        }
+        
+        //일단 다 넣고
+        while(tmp!=null){
+            storage.add(tmp);
+            tmp = tmp.next;
+        }
+        
+        // 특정아이를 빼고
+        len = storage.size();
+        storage.remove(len-n);
+        len = storage.size();
+        
+        // 다시 링크드를 만듦 
+        head = storage.get(0);
+        head.next = null;
+        prev = head;
+        
+        for(int i=len-1; i>0; i--) {
+            prev = head.next;
+            next = storage.get(i);
+            // next.next = null;
+            next.next = prev;
+            head.next = next;
+        }
+        
+        return head;
+    }
+}
