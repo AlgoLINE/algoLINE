f5f59f954b2314592b1fae3bba304518daba1180
wooq <wooq@nhnnext.org>
2015-05-31 01:36:50 +0900

    changed to iterative code

--- 09_June_1st/Gyeongwook/numTrees.py
+++ 09_June_1st/Gyeongwook/numTrees.py
@@ -5,17 +5,11 @@ class Solution:
         lookupTable = [None] * (n+1)
         lookupTable[0] = 1
         
-        return self.subTrees(1, n, lookupTable)		
-    	
-    def subTrees(self, start, end, lookupTable):
-        count = end - start + 1
+        for count in range(1, n+1):
+            cases = 0
+            for root in range(count):
+                cases += lookupTable[root] * lookupTable[count-root-1]
+            
+            lookupTable[count] = cases
         
-        if lookupTable[count] is not None:
-            return lookupTable[count]
-        
-        cases = 0
-        for root in range(1, count+1):
-            cases += self.subTrees(1, root-1, lookupTable) * self.subTrees(root+1, count, lookupTable)
-        
-        lookupTable[count] = cases
-        return cases
+        return lookupTable[n]
