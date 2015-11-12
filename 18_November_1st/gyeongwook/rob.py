9fdf3e5bf300f596a7b6b8e6acdfa62b5824e404
Gyeongwook Choi <gyeongwook.choi@linecorp.com>
2015-11-12 17:57:09 +0900

    성능 안 좋아서 고쳤으나 여전히 안 좋음

--- 18_November_1st/gyeongwook/rob.py
+++ 18_November_1st/gyeongwook/rob.py
@@ -5,23 +5,18 @@ class Solution(object):
         length = len(nums)
         if length == 0: return 0
         
-        lookup_table = []
-        for column in range(length):
-            lookup_table.append([0] * length)
+        self.lookup_table = {}
+        return self.recursion(nums)
         
-        for segment_len in range(length):
-            for start_idx in range(length-segment_len):
-                end_idx = start_idx+segment_len
-                
-                if start_idx == end_idx:
-                    lookup_table[start_idx][end_idx] = nums[start_idx]
-                else:
-                    for pivot in range(segment_len+1):
-                        if pivot == 0:
-                            lookup_table[start_idx][end_idx] = max(lookup_table[start_idx][end_idx], lookup_table[start_idx+1][end_idx])
-                        elif pivot == segment_len:
-                            lookup_table[start_idx][end_idx] = max(lookup_table[start_idx][end_idx], lookup_table[start_idx][end_idx-1])
-                        else:
-                            lookup_table[start_idx][end_idx] = max(lookup_table[start_idx][end_idx], lookup_table[start_idx][start_idx+pivot-1]+lookup_table[start_idx+pivot+1][end_idx])
-                            
-        return lookup_table[0][length-1]
+    def recursion(self, nums):
+        if nums is None: return 0
+        
+        length = len(nums)
+        if length == 0: return 0
+        
+        if length in self.lookup_table:
+            return self.lookup_table[length]
+        else:
+            ret = max(self.recursion(nums[1:], self.recursion(nums[2:]) + nums[0]))
+            self.lookup_table[length] = ret
+            return ret
